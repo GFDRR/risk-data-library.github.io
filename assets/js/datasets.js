bf28f0c1566987a4bc3ac8abcdb5375361afa000
liaa2 <36367920+liaa2@users.noreply.github.com>
2020-05-04 16:00:18 +1000

    create sample dataset component

---
+++ assets/js/datasets.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $.get("https://d3utuyt0gg.execute-api.ap-southeast-2.amazonaws.com/dev", function (data) {
+    data.data.forEach(element => {
+      console.log(element);
+      $("#datasets").append(`
+      <li>
+        <h4>${element.title}</h4>
+        <p>${element.description}</p>
+        <a href="${element.download_link}" download>
+          Download ${element.schema} dataset
+          ${element.file_size}
+        </a>  
+      </li>`)
+    });
+  });
+});
