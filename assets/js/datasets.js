adaf3fc6d52a98d94532ca5d0151838bc4a431d8
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2020-05-05 15:49:19 +1000

    Data page: add initial js to talk to api

--- assets/js/datasets.js
+++ assets/js/datasets.js
@@ -2,15 +2,31 @@ $(document).ready(function () {
   $.get("https://d3utuyt0gg.execute-api.ap-southeast-2.amazonaws.com/dev/api/samples", function (data) {
     data.data.forEach(element => {
       console.log(element);
-      $("#datasets").append(`
+      $("#samples").append(`
       <li>
         <h4>${element.title}</h4>
         <p>${element.description}</p>
         <a href="${element.download_link}" download>
           Download ${element.schema} dataset
           ${element.file_size}
-        </a>  
+        </a>
       </li>`)
+    });
+  });
+});
+
+$(document).ready(function () {
+  $.get("https://d3utuyt0gg.execute-api.ap-southeast-2.amazonaws.com/dev/api/datasets", function (data) {
+    data.data.forEach(element => {
+      console.log(element);
+      $("#datasets").append(`
+        <h4>${element.title}</h4>
+        <p>${element.description}</p>
+        <a href="https://risk-data-library-storage.s3-ap-southeast-2.amazonaws.com/${element.filename}" download>
+          Download ${element.hazard} dataset
+          ${element.size} bytes
+        </a>
+        <pre>${JSON.stringify(element, null, 2)}</pre>`)
     });
   });
 });
