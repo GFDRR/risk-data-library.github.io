8870bf6725a993308abe71c69028389a205c4a65
liaa2 <36367920+liaa2@users.noreply.github.com>
2020-05-06 15:43:06 +1000

    add vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>HELLO from VUE</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style lang="scss" scoped>
+$gray: darkslategrey;
+h1 {
+  color: $gray;
+  font-size: 2rem;
+}
+</style>
