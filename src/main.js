aae59fc7cfdbf51ba0521bbb32d8df7dc29dcd9c
liaa2 <36367920+liaa2@users.noreply.github.com>
2020-05-08 13:11:10 +1000

    remove uncessary vue component

--- src/main.js
+++ src/main.js
@@ -1,41 +1,8 @@
 import Vue from 'vue';
-// import axios from "axios";
-// import App from "./App";
 import DataList from "./components/DataList";
 
 Vue.component("dataList", DataList);
-// Vue.component("app", {
-//   template: DataList,
-//   data: function () {
-//     return {
-//       message: this.results
-//     }
-//   }})
-
-// const url =
-//   "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR";
 
 const vm = new Vue({
   el: "#app",
 });
-
-// const vm = new Vue({
-//   el: "#app",
-//   data: {
-//     results: [],
-//   },
-//   mounted() {
-//     axios.get(url).then((response) => {
-//       console.log("response-->", response);
-
-//       this.results = response.data;
-//     });
-//   },
-//   render: (h) =>
-//     h(DataList, {
-//       props: {
-//         data: "New header text",
-//       },
-//     }),
-//   // render: (h) => h(App),
-// });
