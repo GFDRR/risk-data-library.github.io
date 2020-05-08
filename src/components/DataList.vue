7674bd0eba37b2c1f2fb06077234f9cd6e543361
liaa2 <36367920+liaa2@users.noreply.github.com>
2020-05-08 10:55:10 +1000

    hazard dataset done

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -3,17 +3,49 @@
     <h2>Hazard Datasets</h2>
     <ul v-for="hazardEvent in hazardDataset" :key="hazardEvent.id">
       <li>
-        <div>
-          {{ hazardEvent.title }}
+        <div class="table">
+          <div class="table-header">
+            <h3 class="table-header-title">
+              {{ hazardEvent.title }}
+            </h3>
+            <div>
+              <a class="table-header-redirect">More infomation →</a>
+              <a href="" download="" class="table-header-download">Download ↓</a>
+            </div>
+          </div>
+          <div class="table-body">
+            <div class="table-cell">
+              <p class="table-value">{{hazardEvent.hazard}}</p>
+              <p class="table-subtitle">Hazard</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">{{hazardEvent.region}}</p>
+              <p class="table-subtitle">Region</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">{{hazardEvent.created}}</p>
+              <p class="table-subtitle">Created</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">{{hazardEvent.publisher}}</p>
+              <p class="table-subtitle">Publisher</p>
+            </div>
+             <div class="table-cell">
+              <p class="table-value">{{hazardEvent.licence}}</p>
+              <p class="table-subtitle">Licence</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">{{hazardEvent.fileType}}</p>
+              <p class="table-subtitle">File type</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">{{hazardEvent.fileSize}}</p>
+              <p class="table-subtitle">File size</p>
+            </div>
+          </div>
         </div>
       </li>
     </ul>
-    <!-- <h2>Exposure Datasets</h2>
-    <section id="exposure-datasets"></section>
-    <h2>Vulnerability Datasets</h2>
-    <section id="vulnerability-datasets"></section>
-    <h2>Loss Datasets</h2>
-    <section id="loss-datasets"></section> -->
   </span>
 </template>
 
@@ -38,7 +70,6 @@ export default {
   },
   created () {
     this.siteData = JSON.parse(JSON.stringify(siteData.data));
-    console.log("siteData.data-->", this.siteData);
     for (const key in this.siteData) {
       switch (key) {
         case HAZARD:
@@ -68,6 +99,15 @@ h2 {
   font-size: $quaternary-font-size;
   font-weight: $secondary-font-weight;
   color: $purple-primary-font-color;
+  padding-bottom: 57px;
+  padding-top: 91px;
+}
+
+h3 {
+  font-family: $primary-font;
+  font-size: $primary-font-size;
+  font-weight: bold;
+  color: $dark-secondary-font-color;
 }
 
 ul {
@@ -75,4 +115,54 @@ ul {
   padding: 0;
   margin: 0;
 }
+
+.table {
+  background-color: $light-grey-background;
+  padding: 13px 19px 19px;
+  margin-bottom: 23px;
+}
+
+.table-header, .table-body {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.table-header {
+  border-bottom: 1px solid $divider-lines;
+}
+
+.table-header-redirect {
+  font-weight: $secondary-font-weight;
+  color: $grey-secondary-font-color;
+  margin-right: 48px;
+}
+
+.table-header-download, .table-value, .table-subtitle, .table-header-redirect {
+  font-family: $secondary-font;
+  font-size: $small-font-size;
+}
+
+.table-cell {
+  padding-top: 21px;
+}
+
+.table-header-download {
+  font-weight: $secondary-font-weight;
+  color: $purple-tertiary-font-color;
+}
+
+.table-value, .table-subtitle  {
+  font-weight: $normal-font-weight;
+}
+
+.table-value {
+  color: $dark-secondary-font-color;
+}
+
+.table-subtitle {
+  color: $grey-tertiary-font-color;
+}
+
+
 </style>
