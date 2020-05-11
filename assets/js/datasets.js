cd7239504df1d01ea9168eff7e1d14767bffdb74
liaa2 <36367920+liaa2@users.noreply.github.com>
2020-05-11 13:02:45 +1000

    add datalist page

--- assets/js/datasets.js
+++ assets/js/datasets.js
@@ -1,8 +1,96 @@
 $(document).ready(function () {
+  const HAZARD = "hazard";
+  const EXPOSURE = "exposure";
+  const VULNERABILITY = "vulnerability";
+  const LOSS = "loss";
+
   $.get("/api/datasets.json", function (res) {
-    $("#hazard-datasets").append(`<pre>${JSON.stringify(res.data.hazard, null, 2)}</pre>`);
-    $("#exposure-datasets").append(`<pre>${JSON.stringify(res.data.exposure, null, 2)}</pre>`);
-    $("#vulnerability-datasets").append(`<pre>${JSON.stringify(res.data.vulnerability, null, 2)}</pre>`);
-    $("#loss-datasets").append(`<pre>${JSON.stringify(res.data.loss, null, 2)}</pre>`);
+    let hazardDatasets = null;
+    let exposureDatasets = null;
+    let vulnerabilityDatasets = null;
+    let lossDatasets = null;
+
+    const siteData = JSON.parse(JSON.stringify(res.data));
+    for (const key in siteData) {
+      switch (key) {
+        case HAZARD:
+          hazardDatasets = siteData[`${HAZARD}`]
+          break;
+        case EXPOSURE:
+          exposureDatasets = siteData[`${EXPOSURE}`]
+          break;
+        case VULNERABILITY:
+          vulnerabilityDatasets = siteData[`${VULNERABILITY}`]
+          break;
+        case LOSS:
+          lossDatasets = siteData[`${LOSS}`]
+          break;
+        default:
+          break;
+      }
+    } 
+
+    function appendData(event) {
+      return `
+      <li>
+        <div class="table">
+          <div class="table-header">
+            <h3 class="table-header-title">
+              ${event.title}
+            </h3>
+            <div>
+              <a class="table-header-redirect">More information →</a>
+              <a href="${event.fileLink}" download class="table-header-download">Download ↓</a>
+            </div>
+          </div>
+          <div class="table-body">
+            <div class="table-cell">
+              <p class="table-value">${event.hazard}</p>
+              <p class="table-subtitle">Hazard</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">${event.region}</p>
+              <p class="table-subtitle">Region</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">${event.created}</p>
+              <p class="table-subtitle">Created</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">${event.publisher}</p>
+              <p class="table-subtitle">Publisher</p>
+            </div>
+             <div class="table-cell">
+              <p class="table-value">${event.licence}</p>
+              <p class="table-subtitle">Licence</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">${event.fileType}</p>
+              <p class="table-subtitle">File type</p>
+            </div>
+            <div class="table-cell">
+              <p class="table-value">${event.fileSize}</p>
+              <p class="table-subtitle">File size</p>
+            </div>
+          </div>
+        </div>
+      </li>`
+    }
+
+    $.each(hazardDatasets, function (key, hazardEvent){
+      $("#hazard-datasets").append(appendData(hazardEvent));
+    });
+
+    $.each(exposureDatasets, function (key, exposureEvent) {
+      $("#exposure-datasets").append(appendData(exposureEvent));
+    });
+
+    $.each(vulnerabilityDatasets, function (key, vulnerabilityEvent) {
+      $("#vulnerability-datasets").append(appendData(vulnerabilityEvent));
+    });
+
+    $.each(lossDatasets, function (key, lossEvent) {
+      $("#loss-datasets").append(appendData(lossEvent));
+    });
   });
 });
