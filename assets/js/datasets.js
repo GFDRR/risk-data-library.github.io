b61626e1bcda1ea0dc7b98f1a5f5b8b63c5b6c02
liaa2 <36367920+liaa2@users.noreply.github.com>
2020-05-21 11:44:29 +1000

    remove mermaid; remove brackets & filter out null value

--- assets/js/datasets.js
+++ assets/js/datasets.js
@@ -86,10 +86,26 @@ $(document).ready(function () {
     }
 
     function displayKeyValue(key, dataset) {
-      return key == "year_developed" 
-        ? new Date(dataset).toLocaleString("en-us", { year: "numeric", month: "short", })
-          : transformDataValue(dataset)
+      switch (key) {
+        case "year_developed":
+          return new Date(dataset).toLocaleString("en-us", {
+            year: "numeric",
+            month: "short",
+          });
+        case "exposure_type":
+          return dataset && dataset
+            .replace("(", "")
+            .replace(")", "")
+            .split(",")
+            .filter(function(data) {
+              return data.length > 0;
+            })
+            .join(", ");
+        default:
+          return transformDataValue(dataset);
+      }
     }
+
 
 
     function render(dataset, schema) {
