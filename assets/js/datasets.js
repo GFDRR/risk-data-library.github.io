5ec2f275ce2cde11aac29aaae11b04091c2f9a3d
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2020-05-26 11:42:29 +1000

    Dataset: filter by id

--- assets/js/datasets.js
+++ assets/js/datasets.js
@@ -3,18 +3,7 @@ $(document).ready(function () {
   const EXPOSURE = "exposure";
   const VULNERABILITY = "vulnerability";
   const LOSS = "loss";
-  const METADATA_FIELDS = [
-    // "id",
-    "dataset_name",
-    "location",
-    "hazard_type",
-    "analysis_type",
-    "exposure_type",
-    "function_type",
-    "developed_by",
-    "year_developed",
-    "license"
-  ];
+  
 
   $("#hazard-datasets").append(
     "<p class='loading-text details-content'>Loading....</p>"
@@ -29,149 +18,187 @@ $(document).ready(function () {
     "<p class='loading-text details-content'>Loading....</p>"
   );
 
-  const BASE_URL = "https://d3utuyt0gg.execute-api.ap-southeast-2.amazonaws.com/dev";
 
-  $.get(BASE_URL+"/datasets", function (data) {
-    let hazardDatasets = null;
-    let exposureDatasets = null;
-    let vulnerabilityDatasets = null;
-    let lossDatasets = null;
 
-    const siteData = JSON.parse(JSON.stringify(data));
-    // need var here as IE11 doesn't support const/let for in loop
-    for (var key in siteData) {
-      switch (key) {
-        case HAZARD:
-          hazardDatasets = siteData[HAZARD]
-          break;
-        case EXPOSURE:
-          exposureDatasets = siteData[EXPOSURE]
-          break;
-        case VULNERABILITY:
-          vulnerabilityDatasets = siteData[VULNERABILITY]
-          break;
-        case LOSS:
-          lossDatasets = siteData[LOSS]
-          break;
-        default:
-          break;
+  function update(url) {
+    $.get(url, function (data) {
+      let hazardDatasets = null;
+      let exposureDatasets = null;
+      let vulnerabilityDatasets = null;
+      let lossDatasets = null;
+  
+      const siteData = JSON.parse(JSON.stringify(data));
+      // need var here as IE11 doesn't support const/let for in loop
+      for (var key in siteData) {
+        switch (key) {
+          case HAZARD:
+            hazardDatasets = siteData[HAZARD]
+            break;
+          case EXPOSURE:
+            exposureDatasets = siteData[EXPOSURE]
+            break;
+          case VULNERABILITY:
+            vulnerabilityDatasets = siteData[VULNERABILITY]
+            break;
+          case LOSS:
+            lossDatasets = siteData[LOSS]
+            break;
+          default:
+            break;
+        }
+      } 
+  
+      function getHeadersFromData(dataset) {
+        const keysFromDataset = Object.keys(dataset);
+        
+        const header = METADATA_FIELDS.map(function(key) {
+            if (keysFromDataset.indexOf(key) !== -1) {
+              return (
+                "<th class='data-table-cell data-table-header'>" +
+                key.toUpperCase().replace("_", " ") +
+                "</th>"
+              );
+            }
+          return "";
+        });
+        return "<tr class='data-table-header-container'>"
+            + header.join('')
+            + "<th class='data-table-cell'></th>"
+            + "<th class='data-table-cell'></th>"
+          + "</tr>"
       }
-    } 
-
-    function getHeadersFromData(dataset) {
-      const keysFromDataset = Object.keys(dataset);
-      
-      const header = METADATA_FIELDS.map(function(key) {
+  
+      function transformDataValue(data) {
+        switch (data) {
+          case "":
+          case null:
+            return "-";
+          default:
+            return data;
+        }
+      }
+  
+      function displayKeyValue(key, dataset) {
+        switch (key) {
+          case "year_developed":
+            return new Date(dataset).toLocaleString("en-us", {
+              year: "numeric",
+              month: "short",
+            });
+          case "exposure_type":
+            if (dataset === null || dataset == '') {
+              return '-'
+            }
+            return dataset
+              .replace("(", "")
+              .replace(")", "")
+              .split(",")
+              .filter(function(data) {
+                return data.length > 0;
+              })
+              .join(", ");
+          default:
+            return transformDataValue(dataset);
+        }
+      }
+  
+  
+      const METADATA_FIELDS = [
+        "dataset_name", "location", "hazard_type", "analysis_type",
+        "exposure_type", "function_type", "developed_by", "year_developed",
+        "license"
+      ];
+      function render(dataset, schema) {
+        const keysFromDataset = Object.keys(dataset);
+        const metadata = METADATA_FIELDS.map(function(key) {
           if (keysFromDataset.indexOf(key) !== -1) {
             return (
-              "<th class='data-table-cell data-table-header'>" +
-              key.toUpperCase().replace("_", " ") +
-              "</th>"
+              "<td class='data-table-value data-table-cell'>" +
+              displayKeyValue(key, dataset[key]) +
+              "</td>"
             );
           }
-        return "";
-      });
-      return "<tr class='data-table-header-container'>"
-          + header.join('')
-          + "<th class='data-table-cell'></th>"
-          + "<th class='data-table-cell'></th>"
-        + "</tr>"
-    }
-
-    function transformDataValue(data) {
-      switch (data) {
-        case "":
-        case null:
-          return "-";
-        default:
-          return data;
+          return "";
+        });
+        
+        return (
+          "<tr>" +
+          metadata.join("") +
+          "<td class='data-table-value data-table-cell'><a class='table-header-redirect data-table-value' href='/data-details#" +
+          schema +
+          "=" +
+          dataset.id +
+          "'" +
+          "id='" +
+          dataset.id +
+          "'>More→</a></td>" +
+          "<td class='data-table-value data-table-cell'><a href='" +
+          dataset.download_link +
+          "' download target='_blank'>" +
+          "<img src='assets/images/download_icon.png' class='table-download-link'></a></td>" +
+          "</tr>"
+        );
       }
-    }
-
-    function displayKeyValue(key, dataset) {
-      switch (key) {
-        case "year_developed":
-          return new Date(dataset).toLocaleString("en-us", {
-            year: "numeric",
-            month: "short",
-          });
-        case "exposure_type":
-          if (dataset === null || dataset == '') {
-            return '-'
-          }
-          return dataset
-            .replace("(", "")
-            .replace(")", "")
-            .split(",")
-            .filter(function(data) {
-              return data.length > 0;
-            })
-            .join(", ");
-        default:
-          return transformDataValue(dataset);
-      }
-    }
-
-
-
-    function render(dataset, schema) {
-      const keysFromDataset = Object.keys(dataset);
-      const metadata = METADATA_FIELDS.map(function(key) {
-        if (keysFromDataset.indexOf(key) !== -1) {
-          return (
-            "<td class='data-table-value data-table-cell'>" +
-            displayKeyValue(key, dataset[key]) +
-            "</td>"
-          );
-        }
-        return "";
-      });
+  
+      $("p.loading-text").remove();
       
-      return (
-        "<tr>" +
-        metadata.join("") +
-        "<td class='data-table-value data-table-cell'><a class='table-header-redirect data-table-value' href='/data-details#" +
-        schema +
-        "=" +
-        dataset.id +
-        "'" +
-        "id='" +
-        dataset.id +
-        "'>More→</a></td>" +
-        "<td class='data-table-value data-table-cell'><a href='" +
-        dataset.download_link +
-        "' download target='_blank'>" +
-        "<img src='assets/images/download_icon.png' class='table-download-link'></a></td>" +
-        "</tr>"
-      );
-    }
+      $("#hazard-datasets").append(getHeadersFromData(hazardDatasets[0]));
+  
+      $.each(hazardDatasets, function (key, hazardEvent){
+        $("#hazard-datasets").append(render(hazardEvent, HAZARD));
+      });
+  
+      $("#exposure-datasets").append(getHeadersFromData(exposureDatasets[0]));
+  
+      $.each(exposureDatasets, function (key, exposureEvent) {
+        $("#exposure-datasets").append(render(exposureEvent, EXPOSURE));
+      });
+  
+      $("#vulnerability-datasets").append(getHeadersFromData(vulnerabilityDatasets[0]));
+  
+      $.each(vulnerabilityDatasets, function (key, vulnerabilityEvent) {
+        $("#vulnerability-datasets").append(
+          render(vulnerabilityEvent, VULNERABILITY)
+        );
+      });
+  
+      $("#loss-datasets").append(getHeadersFromData(lossDatasets[0]));
+  
+      $.each(lossDatasets, function (key, lossEvent) {
+        $("#loss-datasets").append(render(lossEvent, LOSS));
+      });
+    });
+  }
 
-    $("p.loading-text").remove();
+  const BASE_URL = "https://d3utuyt0gg.execute-api.ap-southeast-2.amazonaws.com/dev";
+  var GET_SAMPLE_DATASET_URL = BASE_URL + '/datasets?';
+  $.get("/api/samples.json", function (res) {
+    const hazardIds = res[HAZARD].map(function(json) {
+      return json.id;
+    }).join(",");
+
+    const exposureIds = res[EXPOSURE].map(function(json) {
+      return json.id;
+    }).join(",");
+
+    const vulnerabilityIds = res[VULNERABILITY].map(function(json) {
+      return json.id;
+    }).join(",");
+
+    const lossIds = res[LOSS].map(function(json) {
+      return json.id;
+    }).join(",");
     
-    $("#hazard-datasets").append(getHeadersFromData(hazardDatasets[0]));
+    GET_SAMPLE_DATASET_URL = 
+    [
+      BASE_URL,
+      '/datasets?',
+      HAZARD, '=', hazardIds, '&',
+      EXPOSURE, '=', exposureIds, '&',
+      LOSS, '=', lossIds, '&',
+      VULNERABILITY, '=', vulnerabilityIds
+    ].join('')
 
-    $.each(hazardDatasets, function (key, hazardEvent){
-      $("#hazard-datasets").append(render(hazardEvent, HAZARD));
-    });
-
-    $("#exposure-datasets").append(getHeadersFromData(exposureDatasets[0]));
-
-    $.each(exposureDatasets, function (key, exposureEvent) {
-      $("#exposure-datasets").append(render(exposureEvent, EXPOSURE));
-    });
-
-    $("#vulnerability-datasets").append(getHeadersFromData(vulnerabilityDatasets[0]));
-
-    $.each(vulnerabilityDatasets, function (key, vulnerabilityEvent) {
-      $("#vulnerability-datasets").append(
-        render(vulnerabilityEvent, VULNERABILITY)
-      );
-    });
-
-    $("#loss-datasets").append(getHeadersFromData(lossDatasets[0]));
-
-    $.each(lossDatasets, function (key, lossEvent) {
-      $("#loss-datasets").append(render(lossEvent, LOSS));
-    });
+    update(GET_SAMPLE_DATASET_URL);
   });
 });
